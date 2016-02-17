9c738655e557bcf24905771eac58f845fcd1dacb
fcc-fab-lam <fabien.lamon@gmail.com>
2016-02-17 11:36:14 +0100

    routes

--- app/routes.php
+++ app/routes.php
@@ -6,17 +6,17 @@
 		['GET|POST', '/resultats-recherche', 'Default#searchResults', 'search-results'],// profil utilsateur
 		['GET|POST', '/a-propos','Default#aboutUs', 'about-us'],// qui sommes-nous
 		['GET|POST', '/selection-film[:id]', 'Default#selectionMovie', 'selection-movie'], // film selectionné
-		['GET|POST', '/profil-utilisateur', 'Back#userProfil', 'user-profil'], // Profil de l'utilisateur
-		['GET|POST', '/modification-profil', 'Back#updateProfil', 'update-profil'], // Modif profil de l'utilisateur
-		['GET|POST', '/votre-cave', 'Back#cave', 'cave'], // liste des choix de l'utilisateur(cave)
-		['GET|POST', '/ajouter-commmentaire', 'Back#addComments', 'add-comments'], // Ajouter un commentaire
-		['GET|POST', '/association-film-vin', 'Back#associationMovieWine', 'association-movie-wine'], // association 1 film et 1 vin
-		['GET|POST', '/ajout-fiche-vin', 'Back#addWine', 'add-wine'], // creation fiche vin
-		['GET|POST', '/ajout-genre-vin', 'Back#addWineGenre', 'add-wine-genre'], // Ajout genre de vin
-		['GET|POST', '/ajout-genre-film', 'Back#addMovieGenre', 'add-movie-genre'], // Ajout genre de film
-		['GET|POST', '/association-genres', 'Back#associationGenres', 'association-genres'], // Association 1 genre de film et 1 genre de vin 
-		['GET|POST', '/liste-associations-non-moderees', 'Back#listNotModeratedAssociations', 'list-not-moderated-associations'], // Liste des associations non-modérées 
-		['GET|POST', '/liste-commentaires-non-moderees', 'Back#listNotModeratedComments', 'list-not-moderated-comments'], // Liste des commentaires non-moderés 
-		['GET|POST', '/formulaire-moderation', 'Back#moderationForm', 'moderation-form'], // Formulaire de moderation  
-		['GET|POST', '/desactiver-compte', 'Back#disableAccount', 'disable-acccount'], // Desactiver un compte
+		['GET|POST', '/profil-utilisateur', 'Admin#userProfil', 'user-profil'], // Profil de l'utilisateur
+		['GET|POST', '/modification-profil', 'Admin#updateProfil', 'update-profil'], // Modif profil de l'utilisateur
+		['GET|POST', '/votre-cave', 'Admin#cave', 'cave'], // liste des choix de l'utilisateur(cave)
+		['GET|POST', '/ajouter-commmentaire', 'Admin#addComments', 'add-comments'], // Ajouter un commentaire
+		['GET|POST', '/association-film-vin', 'Admin#associationMovieWine', 'association-movie-wine'], // association 1 film et 1 vin
+		['GET|POST', '/ajout-fiche-vin', 'Admin#addWine', 'add-wine'], // creation fiche vin
+		['GET|POST', '/ajout-genre-vin', 'Admin#addWineGenre', 'add-wine-genre'], // Ajout genre de vin
+		['GET|POST', '/ajout-genre-film', 'Admin#addMovieGenre', 'add-movie-genre'], // Ajout genre de film
+		['GET|POST', '/association-genres', 'Admin#associationGenres', 'association-genres'], // Association 1 genre de film et 1 genre de vin 
+		['GET|POST', '/liste-associations-non-moderees', 'Admin#listNotModeratedAssociations', 'list-not-moderated-associations'], // Liste des associations non-modérées 
+		['GET|POST', '/liste-commentaires-non-moderees', 'Admin#listNotModeratedComments', 'list-not-moderated-comments'], // Liste des commentaires non-moderés 
+		['GET|POST', '/formulaire-moderation', 'Admin#moderationForm', 'moderation-form'], // Formulaire de moderation  
+		['GET|POST', '/desactiver-compte', 'Admin#disableAccount', 'disable-acccount'], // Desactiver un compte
 	);
