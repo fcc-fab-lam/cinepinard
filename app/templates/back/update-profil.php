e0a7b0a5e42e5c6e18a222e04ed37e9a80406c80
lilithxxx <lilith.emy@gmail.com>
2016-02-17 11:05:48 +0100

    creation page update profil

---
+++ app/templates/back/update-profil.php
@@ -0,0 +1,10 @@
+<?php $this->layout('layout', ['title' => 'Inscription']) ?>
+
+<?php $this->start('main_content') ?>
+
+<!-- DEBUT FORMULAIRE CHANGEMENT DE PROFIL -->
+
+
+<!-- FIN FORMULAIRE -->
+
+<?php $this->stop('main_content') ?>
