42c19dca959ab5f8175d0e7cbadc750c0eda260e
fcc-fab-lam <fabien.lamon@gmail.com>
2016-02-17 09:26:11 +0100

    déplacement fichiers templates

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -102,26 +102,26 @@ class UsersController extends Controller
 	// Profil de l'utilisateur
 	public function userProfil()
 	{
-		$this->show('users/user-profil', ['showErr' => $showErr, 'err' => $err]);
+		$this->show('back/user-profil', ['showErr' => $showErr, 'err' => $err]);
 	}
 
 	// Modif profil de l'utilisateur
 
 	public function updateProfil()
 	{
-		$this->show('users/update-profil', ['showErr' => $showErr, 'err' => $err]);
+		$this->show('back/update-profil', ['showErr' => $showErr, 'err' => $err]);
 	}
 
 	// liste des choix de l'utilisateur(cave)
 	public function cave()
 	{
-		$this->show('users/cave', ['showErr' => $showErr, 'err' => $err]);
+		$this->show('back/cave', ['showErr' => $showErr, 'err' => $err]);
 	}
 	
 	// Ajouter un commentaire
 	public function addComments()
 	{
-		$this->show('users/add-comments', ['showErr' => $showErr, 'err' => $err]);
+		$this->show('back/add-comments', ['showErr' => $showErr, 'err' => $err]);
 	}
 
 
