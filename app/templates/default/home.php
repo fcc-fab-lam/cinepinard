fb040e0e57a3ab481edd700176a21b49a7caa502
paulincrtn <paulin.crtn@gmail.com>
2016-02-22 12:24:14 +0100

    CSS PAGE ACCUEIL ET RECHERCHE

--- app/templates/default/home.php
+++ app/templates/default/home.php
@@ -6,34 +6,39 @@
 <!-- DEBUT RECHERCHE -->
 <div class="recherche">
 	<div class="container-fluid">
-		<div class="row searchfilm">
 		<h1>Recherche d'un film</h1>
-			<form action="resultats-recherche">
-				<input type="text" placeholder="Ex : Deadpool" name="film">
+		<form action="resultats-recherche" class="row">
+			
+			<div class="col-lg-4 col-md-12">
+				<input class="col-lg-12" type="text" placeholder="Ex : Deadpool" name="film">
+			</div>
 
-				<div class="preferences">J'aime :
-					<?php 
-                        foreach($categories as $value): 
-                            $userCat = array();
-                            foreach ($userPrefs as $value) {
-                                $userCat[] = $value['categorie_id'];
-                            };
-                    ?>
-						<label><input type="checkbox" name="preferences[]" value="<?=$value['id']; ?>" <?php if(in_array($value['id'], $userCat)){echo 'checked="checked"';} ?> /><?=$value['name']; ?></label>
-					<?php endforeach; ?>
-				</div>
+			<div class="preferences col-lg-6 col-md-12">
+				<h2>J'aime :</h2>
+				<?php 
+	                foreach($categories as $value): 
+	                    $userCat = array();
+	                    foreach ($userPrefs as $value) {
+	                        $userCat[] = $value['categorie_id'];
+	                    };
+	            ?>
+					<label><input type="checkbox" name="preferences[]" value="<?=$value['id']; ?>" <?php if(in_array($value['id'], $userCat)){echo 'checked="checked"';} ?> /><?= ucfirst($value['name']); ?></label>
+				<?php endforeach; ?>
+			</div>
 
-				<input type="submit" value="recherche" />
-			</form>
-			<?php if(isset($_SESSION['listErr'])) : ?>
-				<div class="erreurs">
-					<?php 
-						echo implode('<br/>', $_SESSION['listErr']);
-						unset($_SESSION['listErr']);
-					?>
-				</div>
-			<?php endif; ?>
-		</div>
+			<div class="col-lg-2 col-md-12">
+				<input class="col-lg-12" type="submit" value="Je bois !" />
+			</div>
+		</form>
+
+		<?php if(isset($_SESSION['listErr'])) : ?>
+			<div class="erreurs">
+				<?php 
+					echo implode('<br/>', $_SESSION['listErr']);
+					unset($_SESSION['listErr']);
+				?>
+			</div>
+		<?php endif; ?>
 	</div>
 </div>
 <!-- FIN RECHERCHE -->
--- app/templates/default/search-results.php
+++ app/templates/default/search-results.php
@@ -1,7 +1,7 @@
 <?php $this->layout('layout', ['title' => 'Résultats de recherche']) ?>
 
 <?php $this->start('main_content') ?>
-<div>
+<div class="container-fluid background-recherche">
 	<?php
 	
 	$userPrefs = '';
@@ -14,25 +14,27 @@
 			$userPrefs .= '&preferences[]='.$value;
 		}
 		foreach($resultats as $value): ?>
+			<div class="row resultats">
+				<div class="image-res-seach">
+				<!-- IMAGE -->
+				<p><?= (isset($value->poster->href)) ? '<a href="selection-film?id=<?=$value->code.$userPrefs ?>"><img height="200px" src="'.$value->poster->href.'" /></a>' : '' ?></p>
+				</div>
+				<div class="descriptif-res-search">
+					<!-- TITRE ORIGINAL OU FRANCAIS -->
+					<h4><?= (isset($value->title)) ? $value->title : $value->originalTitle ?></h4>
 
-			<!-- TITRE ORIGINAL OU FRANCAIS -->
-			<h4><?= (isset($value->title)) ? $value->title : $value->originalTitle ?></h4>
+					<!-- REALISATEUR -->
+					<p>Réalisateur : <?= (isset($value->castingShort->directors)) ? $value->castingShort->directors : 'Inconnu' ?></p>
 
-			<!-- REALISATEUR -->
-			<p>Réalisateur : <?= (isset($value->castingShort->directors)) ? $value->castingShort->directors : 'Inconnu' ?></p>
+					<!-- ACTEURS -->
+					<p>Acteurs : <?= (isset($value->castingShort->actors)) ? $value->castingShort->actors : 'Inconnu' ?></p>
 
-			<!-- ACTEURS -->
-			<p>Acteurs : <?= (isset($value->castingShort->actors)) ? $value->castingShort->actors : 'Inconnu' ?></p>
-
-			<!-- ANNEE DE PRODUCTION -->
-			<p>Année : <?= (isset($value->productionYear)) ? $value->productionYear : 'Inconnu' ?></p>
-
-			<!-- IMAGE -->
-			<p><?= (isset($value->poster->href)) ? '<img height="300px" src="'.$value->poster->href.'" />' : '' ?></p>
-
-			<!-- Call to action choix du film -->
-			<a href="selection-film?id=<?=$value->code.$userPrefs ?>">Choisir ce film</a>
-
+					<!-- ANNEE DE PRODUCTION -->
+					<p>Année : <?= (isset($value->productionYear)) ? $value->productionYear : 'Inconnu' ?></p>
+					<!-- Call to action choix du film -->
+					<a href="selection-film?id=<?=$value->code.$userPrefs ?>">Choisir ce film</a>
+				</div>
+			</div>
 		<?php endforeach; 
 	} // Fin du else ?>
 </div>
