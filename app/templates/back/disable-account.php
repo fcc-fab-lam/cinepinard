bf4b0a3f37a4246e22ebb65248bacf8657b4943a
paulincrtn <paulin.crtn@gmail.com>
2016-02-23 11:46:51 +0100

    CSS Disable account

--- app/templates/back/disable-account.php
+++ app/templates/back/disable-account.php
@@ -2,27 +2,32 @@
 
 <?php $this->start('main_content') ?>
 
+<div class="disable-container">
+	<div class="disable">
+		
+	<p>Voulez-vous vraiment effacer votre compte ?</p>
 
-<p>Voulez-vous vraiment effacer votre compte ?</p>
+	<form role="form" class="form-horizontal" method="post" action="" enctype="multipart/form-data">	
+	 	<input type="radio" name="disable" value="oui" id="oui" />
+	 	<label for="oui">Oui</label><br /> 
 
-<form role="form" class="form-horizontal" method="post" action="" enctype="multipart/form-data">	
- 	<input type="radio" name="disable" value="oui" id="oui" />
- 	<label for="oui">Oui</label><br /> 
+	 	<input type="radio" name="disable" value="non" id="non" />
+	 	<label for="non">non</label><br />
 
- 	<input type="radio" name="disable" value="non" id="non" />
- 	<label for="non">non</label><br />
+	 	<input class="btn btn-default" type="submit" value="Validation" />
+	 	
+	</form>
 
- 	<input class="btn btn-default" type="submit" value="Validation" />
- 	
-</form>
+	 	<?php
+	 	// envoyer les erreurs si il y en a
+			if($showErr){
+				echo '<div class="erreurs">';
+				echo implode('<br/>', $err);
+				echo '</div>';
+			}
+		?>
 
- 	<?php
- 	// envoyer les erreurs si il y en a
-		if($showErr){
-			echo '<div class="erreurs">';
-			echo implode('<br/>', $err);
-			echo '</div>';
-		}
-	?>
+	</div>
+</div>
 
 <?php $this->stop('main_content') ?>
