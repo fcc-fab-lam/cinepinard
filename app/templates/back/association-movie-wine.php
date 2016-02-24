b3cd6df598f6af90ef39af0931f10ca2a7ab5c2c
fcc-fab-lam <fabien.lamon@gmail.com>
2016-02-24 18:46:52 +0100

    petit rajout

--- app/templates/back/association-movie-wine.php
+++ app/templates/back/association-movie-wine.php
@@ -45,15 +45,54 @@
                 <div class="col-lg-10 col-md-12">
                     <input class="col-lg-12" type="text" placeholder="Ex : Saint Julien" name="film">
                     <input type="hidden" name="idVin">
-
                 </div>
                 <div class="col-lg-2"></div>
             </div>
             <div class="col-lg-2 col-md-12">
                 <h2 class="col-md-12">&nbsp;</h2>
-                <input class="col-md-12" type="submit" value="Perfect Match" />
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Large modal</button>
             </div>
 		</form>
+        <div class="modal fade" tabindex="-1" role="dialog" id="myModal">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Modal title</h4>
+              </div>
+              <div class="modal-body">
+                <div class="row">
+                  <div class="col-md-4">.col-md-4</div>
+                  <div class="col-md-4 col-md-offset-4">.col-md-4 .col-md-offset-4</div>
+                </div>
+                <div class="row">
+                  <div class="col-md-3 col-md-offset-3">.col-md-3 .col-md-offset-3</div>
+                  <div class="col-md-2 col-md-offset-4">.col-md-2 .col-md-offset-4</div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6 col-md-offset-3">.col-md-6 .col-md-offset-3</div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-9">
+                    Level 1: .col-sm-9
+                    <div class="row">
+                      <div class="col-xs-8 col-sm-6">
+                        Level 2: .col-xs-8 .col-sm-6
+                      </div>
+                      <div class="col-xs-4 col-sm-6">
+                        Level 2: .col-xs-4 .col-sm-6
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div><!-- /.modal-content -->
+          </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
 		<?php if(isset($_SESSION['listErr'])) : ?>
 			<div class="erreurs">
 				<?php 
