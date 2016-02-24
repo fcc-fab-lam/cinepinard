2a94219d81b66b4fee748b26e91f56aacc3b1026
bertrand214 <zoubrouska@hotmail.com>
2016-02-24 11:59:31 +0100

    aboutus

--- app/templates/default/about-us.php
+++ app/templates/default/about-us.php
@@ -5,21 +5,21 @@
 <h1>Qui sommes-nous ?</h1>
 
 
-Mesdames,Mesdemosielles, Messieurs,
+Mesdames,Mesdemoiselles, Messieurs,
 
 
 <p>Passionnés de cinéma, amoureux des vins, nous avons décidé de tisser une toile entre deux mondes où le plaisir est le maître-mot. Un film pour un vin.
 La rencontre de ces deux mondes peut paraître fortuite ou même hasardeuse, mais c'est une évidence. C'est tout d'abord pour nous une aventure ludique où nos palais parlent à nos yeux.
 Il est question de création.  L'un naît de la terre et l'autre de l'esprit. De siècle en siècle, d’évolution en révolution, le vin et le cinéma nous racontent une histoire.  Et, nous adorons les histoires.</p> 
 
-<p>Il est question d’hédonisme. Laissez-vous distraire par la robe d'un vin  tout en laissant vos yeux  glisser sur les jambes de muse  7eme art. Cette rencontre doit être parfaite,  vos papilles et vos iris trouveront une histoire commune pour un scenario de plaisir.</p>
-<p>Il est question de goûts. Nous marions des vins et autres liqueurs aux plus grandes œuvres du cinéma. Prêtre de cette nouvelle religion, nous vous proposons nos histoires et nous attendons avec impatience les vôtres.  Du cinéma d'auteurs au blockbusters,  de la bouteille d’exception au vin de table, ce monde est si vaste que nous avons besoin de missionnaires. Inscrivez-vous,  testez, commentez et proposez- nous vos associations afin de  faire de notre « site », une bible hédoniste et œcuménique.</p>
+<p>Il est question d’hédonisme. Laissez-vous distraire par la robe d'un vin  tout en laissant vos yeux  glisser sur les jambes de muse  7eme art. Cette rencontre doit être parfaite, vos papilles et vos iris trouveront une histoire commune pour un scenario de plaisir.</p>
+<p>Il est question de goûts. Nous marions des vins et autres liqueurs aux plus grandes œuvres du cinéma. Prêtre de cette nouvelle religion, nous vous proposons nos histoires et nous attendons avec impatience les vôtres.  Du cinéma d'auteurs au blockbusters,  de la bouteille d’exception au vin plus genereux, ce monde est si vaste que nous avons besoin de missionnaires. Inscrivez-vous,  testez, commentez et proposez-nous vos associations afin de faire de notre « site », une bible hédoniste et œcuménique.</p>
 
 Vous êtes officiellement invités aux noces de Bacchus et de Muse Cinéma. 
 
-Bienvenue sur  WineScreen ! 
+Bienvenue sur WineScreen ! 
 
- Emilie, Fabien, Paulin et Bertrand.
+Emilie, Fabien, Paulin et Bertrand.
 
 
 <?php $this->stop('main_content') ?>
